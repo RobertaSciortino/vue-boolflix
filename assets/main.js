f0e04e83ba33ed6d096edc4aa07996cb1c30f405
Roberta <robertasciortino.ag@gmail.com>
2020-11-24 13:06:31 +0100

    chiamata ajax al click o al keyup.enter

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,24 @@
 var app = new Vue ({
   el: '#root',
   data: {
-    userSearch: ''
+    userSearch: '',
+    filmsList: []
   },
   methods: {
     //intercetto il click sul button o il keyup invio in input e salvo cio' che ha digitato l'utente
     search() {
       console.log(this.userSearch);
+
+      //chiamata ajax
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: 'f70b7a3263a4bd2bca0c361be3b2b3e3',
+          query: 'joker'
+        }
+      }).then((response) => {
+        this.filmsList = response.data.results;
+        console.log(response.data.results);
+      }); 
     }
   }
 });
