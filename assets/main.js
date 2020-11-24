b56469b4f4eec5dff516baf465126008af51f76b
Roberta <robertasciortino.ag@gmail.com>
2020-11-24 19:13:49 +0100

    chiamata ajax ricerca serie tv

--- assets/main.js
+++ assets/main.js
@@ -3,15 +3,14 @@ var app = new Vue ({
   data: {
     userSearch: '',
     filmsList: [],
-    filmsListLength: 20,
-    maxVote: 5
+    maxVote: 5,
+    seriesList: []
   },
   methods: {
-    //intercetto il click sul button o il keyup invio in input e salvo cio' che ha digitato l'utente
     search() {
+      //intercetto il click sul button o il keyup invio in input e salvo cio' che ha digitato l'utente
       console.log(this.userSearch);
-
-      //chiamata ajax
+      //chiamata ajax film
       axios.get('https://api.themoviedb.org/3/search/movie', {
         params: {
           api_key: 'f70b7a3263a4bd2bca0c361be3b2b3e3',
@@ -20,13 +19,26 @@ var app = new Vue ({
       }).then((response) => {
         this.filmsList = response.data.results;
         console.log(this.filmsList);
-
         //assegno al voto il valore numerico su base 5 corrispondente
         this.filmsList.forEach((film) => {
           film.vote_average = Math.round(film.vote_average / 2);
         });
       });
 
+      //chiamata ajax serieTV
+      axios.get('https://api.themoviedb.org/3/search/tv', {
+        params: {
+          api_key: 'f70b7a3263a4bd2bca0c361be3b2b3e3',
+          query: this.userSearch
+        }
+      }).then((response) => {
+        this.seriesList = response.data.results;
+        console.log(this.seriesList);
+        //assegno al voto il valore numerico su base 5 corrispondente
+        this.seriesList.forEach((serie) => {
+          serie.vote_average = Math.round(serie.vote_average / 2);
+        });
+      });
 
     }
   }
