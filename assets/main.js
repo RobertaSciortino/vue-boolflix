b97a796804c211e8544bca39c47eaa9a9fd5681f
Roberta <robertasciortino.ag@gmail.com>
2020-11-24 14:06:57 +0100

    chiamata ajax ricerca utente

--- assets/main.js
+++ assets/main.js
@@ -13,12 +13,12 @@ var app = new Vue ({
       axios.get('https://api.themoviedb.org/3/search/movie', {
         params: {
           api_key: 'f70b7a3263a4bd2bca0c361be3b2b3e3',
-          query: 'joker'
+          query: this.userSearch
         }
       }).then((response) => {
         this.filmsList = response.data.results;
         console.log(response.data.results);
-      }); 
+      });
     }
   }
 });
