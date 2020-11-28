f05e4a73551ca577d969c5d71827049c94ecc86f
Roberta <robertasciortino.ag@gmail.com>
2020-11-28 17:30:00 +0100

    aggiunta cast e stile scrollbar

--- assets/main.js
+++ assets/main.js
@@ -8,6 +8,8 @@ var app = new Vue ({
     searching: false,
     languages: ['ch', 'de', 'en', 'es', 'fr', 'it', 'ja'],
     searchedTitle:'',
+    castsFilms: [],
+    castsSeries: []
   },
   methods: {
     search() {
@@ -31,6 +33,21 @@ var app = new Vue ({
           this.filmsList = response.data.results;
           console.log(this.filmsList);
           this.searching = false;
+        }).then(() => {
+          this.filmsList.forEach((film) => {
+            axios.get('https://api.themoviedb.org/3/movie/' + film.id + '/credits', {
+              params: {
+                api_key: 'f70b7a3263a4bd2bca0c361be3b2b3e3',
+                movie_id: film.id
+              }
+            }).then((response) => {
+              this.castsFilms.push({
+                id: response.data.id,
+                cast: response.data.cast
+              });
+            });
+          });
+
         });
 
         //chiamata ajax serieTV
@@ -43,6 +60,21 @@ var app = new Vue ({
           this.seriesList = response.data.results;
           console.log(this.seriesList);
           this.searching = false;
+        }).then(() => {
+          this.seriesList.forEach((serie) => {
+            axios.get('https://api.themoviedb.org/3/tv/' + serie.id + '/credits', {
+              params: {
+                api_key: 'f70b7a3263a4bd2bca0c361be3b2b3e3',
+                tv_id: serie.id
+              }
+            }).then((response) => {
+              this.castsSeries.push({
+                id: response.data.id,
+                cast: response.data.cast
+              });
+            });
+          });
+
         });
 
       }
@@ -52,6 +84,17 @@ var app = new Vue ({
     },
     isFlagVisible(film) {
       return this.languages.includes(film.original_language);
+    },
+    getCast(film, castsArray){
+      for (var i = 0; i < castsArray.length; i++) {
+        if(castsArray[i].id == film.id){
+          if(castsArray[i].cast.length > 4){
+            return castsArray[i].cast.slice(0, 5);
+          } else{
+            return castsArray[i].cast;
+          }
+        }
+      }
     }
   }
 
